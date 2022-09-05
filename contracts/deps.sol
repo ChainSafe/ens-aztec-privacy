015f9173a322b22da3a013ae0baa216ca96ee1fe
Jagrut Kosti <jagrut@chainsafe.io>
2022-09-05 14:55:32 +0200

    initial commit

---
+++ contracts/deps.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.9;
+
+// //SPDX-License-Identifier: MIT
+// // These imports are here to force Hardhat to compile contracts we depend on in our tests but don't need anywhere else.
+// import "@ensdomains/ens-contracts/contracts/registry/ENSRegistry.sol";
+// import "@ensdomains/ens-contracts/contracts/registry/FIFSRegistrar.sol";
+// import "@ensdomains/ens-contracts/contracts/resolvers/PublicResolver.sol";
