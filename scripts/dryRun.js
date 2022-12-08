b0b7de6130509793446d2c8a90dbfdc7ef568f2f
Jagrut Kosti <jagrut@chainsafe.io>
2022-12-08 12:56:36 +0530

    modified dryRun for mainnet

--- scripts/dryRun.js
+++ scripts/dryRun.js
@@ -3,21 +3,23 @@ const hre = require("hardhat");
 const ethers = hre.ethers;
 const namehash = require("eth-ens-namehash");
 
+// only for mainnet
 async function main () {
   const node = namehash.hash(process.env.ENS_NAME_FOR_PRIVATE_KEY);
   await enablePrivateTx(node)
-  await sendPrivate(node, ethers.utils.parseEther("0.001"))
-  console.log('Sent!')
+  // uncomment run separately from owner account
+  // await sendPrivate(node, ethers.utils.parseEther("0.001"))
+  console.log('Sent to: ', process.env.ENS_NAME_FOR_PRIVATE_KEY);
 }
 
 async function enablePrivateTx(node) {
-  const CustomResolver = await ethers.getContractAt("CustomResolver", process.env.GOERLI_CUSTOMRESOLVER_ADDR)
+  const CustomResolver = await ethers.getContractAt("CustomResolver", process.env.MAINNET_CUSTOMRESOLVER_ADDR)
   const setSendPrivate = await CustomResolver.setSendPrivate(node, true)
   await setSendPrivate.wait()
 }
 
 async function sendPrivate(node, amount) {
-  const CustomResolver = await ethers.getContractAt("CustomResolver", process.env.GOERLI_CUSTOMRESOLVER_ADDR)
+  const CustomResolver = await ethers.getContractAt("CustomResolver", process.env.MAINNET_CUSTOMRESOLVER_ADDR)
   const tx = await CustomResolver.sendPrivate(node, {
     value: amount
   })
